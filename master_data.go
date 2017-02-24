d9ddf0896fb645a90259c4c5a6657ff9cf00f474
elgs <elgs1980@hotmail.com>
2017-02-24 02:41:52 -0700

    Update.

--- master_data.go
+++ master_data.go
@@ -773,31 +773,19 @@ func (this *Query) Store() error {
 	if app == nil {
 		return errors.New("App not found: " + this.AppId)
 	}
+
+	//	fmt.Println(this.ScriptPath)
+	//	fmt.Println(this.ScriptText)
 	if strings.TrimSpace(this.ScriptPath) == "" {
-		qFileFound := false
-		qFileName := "." + Websql.AppName + "/" + app.Name + "/" + this.Name
-		if _, err := os.Stat(homeDir + "/" + qFileName); !os.IsNotExist(err) {
-			qFileName = homeDir + "/" + qFileName
-			qFileFound = true
-		}
-		if _, err := os.Stat(pwd + "/" + qFileName); !os.IsNotExist(err) {
-			qFileName = pwd + "/" + qFileName
-			qFileFound = true
+
+		appDir := homeDir + "/." + Websql.AppName + "/" + app.Name + "/"
+		err := os.MkdirAll(appDir, 0755)
+		if err != nil {
+			return errors.New("Failed to create directory: " + appDir)
 		}
 
-		if !qFileFound {
-			qFileName += ".sql"
-			if _, err := os.Stat(homeDir + "/" + qFileName); !os.IsNotExist(err) {
-				qFileName = homeDir + "/" + qFileName
-				qFileFound = true
-			}
-			if _, err := os.Stat(pwd + "/" + qFileName); !os.IsNotExist(err) {
-				qFileName = pwd + "/" + qFileName
-				qFileFound = true
-			}
-		}
-
-		err := ioutil.WriteFile(qFileName, []byte(this.ScriptText), 0644)
+		qFileName := appDir + this.Name + ".sql"
+		err = ioutil.WriteFile(qFileName, []byte(this.ScriptText), 0644)
 		if err != nil {
 			return errors.New("Failed to write query file: " + qFileName)
 		}
